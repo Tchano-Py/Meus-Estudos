43390d2e6c4a3e0c0e95c72fb272d8bebd70a0b5
Tchano-Py <tchanopy@gmail.com>
2025-08-15 20:31:23 +0100

    Add: Introdução às variáveis e tipos de dados em Dart(Pratícas)

--- Dart/Aulas/1-introducao-ao-dart.dart
+++ Dart/Aulas/1-introducao-ao-dart.dart
@@ -1,4 +1,19 @@
-// Sintaxe básica do Dart
-void main(){
-  print("Olá, Dart!");
+void main() {
+  //========= Variáveis =========
+  var nome = "João"; // para inferência automática de tipo.
+  Object nomeCompleto = "João Da Silva"; // para aceitar qualquer tipo de valor.
+  String sobreNome =
+      "Da Silva"; // para garantir que só valores do tipo String sejam aceitos.
+
+  print(
+    "Olá, $nome!. Bem-vindo ao Dart! Seu nome completo é $nomeCompleto e seu sobrenome é $sobreNome.",
+  );
+
+  /*========= Curiosidade =========
+  Não, tempo de compilação e tempo de execução são coisas diferentes:
+
+  Tempo de compilação: É o momento em que o código fonte é analisado e convertido em código executável pelo compilador. Erros de sintaxe e de tipo geralmente aparecem aqui.
+
+  Tempo de execução: É quando o programa já está rodando, após ter sido compilado. Erros que dependem do comportamento do programa (como acessar um método que não existe) aparecem aqui.
+  */
 }
---
+++ Dart/Matérias/2-tipos-de-dados-e-variaveis.md
@@ -0,0 +1,1 @@
+# Tipos de dados e variáveis
