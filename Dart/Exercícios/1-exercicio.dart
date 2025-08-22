f3ee292aa82685eb7ea7beba3a92d352cb27910c
Tchano-Py <tchanopy@gmail.com>
2025-08-22 14:31:20 +0100

    fix: exercício 1

---
+++ Dart/Exercícios/1-exercicio.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  /// Nível Fácil: Peça (ou simule com uma variável) a idade de uma pessoa. Se a idade for menor que 18, mostre: "Menor de idade". Caso contrário, mostre: "Maior de idade". Use um if-else simples.
+  
+  /// Nível Médio: Crie um programa que leia (simule) um número de 1 a 7 e mostre o dia da semana correspondente: 1 → Domingo, 2 → Segunda-feira ... 7 → Sábado. Use um switch-case para resolver.
+  
+  /// Nível Difícil: Faça um programa que peça um número inteiro positivo n e mostre a soma de todos os números pares de 1 até n. Exemplo: Se n = 10, saída:
+}
